bd84c06f3270fbd7e7bcc638329f0fa41e5264a5
HXShuai <haoxiaoshuai@126.com>
2019-05-05 09:32:02 +0800

    提交一个项目
    提交一个test项目试试如果有冲突，是怎么解决的
    
    Co-Authored-By: hxshuai <haoxiaoshuai@126.com>

---
+++ CodeLife/CodeLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeLife
+//
+//  Created by GlobalOneEdu on 2019/5/5.
+//  Copyright © 2019 CodeLifeForAnyOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CodeLife/CodeLifeTests/CodeLifeTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodeLifeTests.swift
+//  CodeLifeTests
+//
+//  Created by GlobalOneEdu on 2019/5/5.
+//  Copyright © 2019 CodeLifeForAnyOne. All rights reserved.
+//
+
+import XCTest
+@testable import CodeLife
+
+class CodeLifeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
